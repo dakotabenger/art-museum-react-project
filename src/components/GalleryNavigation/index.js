b3703bc7be237b950933708fa3d992c887abff76
Dakota Benger <dakota@bengerlaw.com>
2020-12-08 15:43:13 -0700

    step 24

--- src/components/GalleryNavigation/index.js
+++ src/components/GalleryNavigation/index.js
@@ -1,8 +1,27 @@
+import {NavLink, Route, Switch} from 'react-router-dom'
+import './GalleryNavigation.css'
+
 export default function GalleryNavigation(props){
    
     const {galleries} = props;
     console.log(galleries)
     return (
-        <h1>Galleries</h1>
+        <nav>
+            
+            <NavLink to="/">
+                <h1>Galleries</h1>
+            </NavLink>
+           {galleries.map(gallery => {
+               return ( <NavLink exact key={gallery.gallerynumber} to={`/galleries/${gallery.gallerynumber}`} >
+                   {gallery.name}
+               </NavLink>
+               )
+            })}
+            <Switch>
+                <Route exact path={"/galleries/:galleryId"}>
+                    <h1>Test</h1>            
+                 </Route>
+            </Switch>
+        </nav>
     )
     }
